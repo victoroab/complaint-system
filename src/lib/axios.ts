0e848a19dc95d05f8ba14f49ce54d405bdf42cf4
victoroab <96608980+victoroab@users.noreply.github.com>
2024-03-04 19:36:17 +0100

    prod test

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL as string
+
 export const Axios = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: BASE_URL,
 })
