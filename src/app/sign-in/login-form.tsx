d01231cb7abefdf01abf2207c940ad2d5d292dfb
victoroab <96608980+victoroab@users.noreply.github.com>
2024-03-14 14:27:05 +0100

    profile page fast load on login

--- src/app/sign-in/login-form.tsx
+++ src/app/sign-in/login-form.tsx
@@ -41,7 +41,6 @@ export function LoginForm({ userType }: { userType: string }) {
     onSuccess() {
       const session = JSON.parse(localStorage.getItem('session_key')!)
       if (session.access_token) {
-        console.log('Hi')
         router.replace('/profile')
       }
     },
