a661217945227fb72d24a98877e2663748afac72
victoroab <96608980+victoroab@users.noreply.github.com>
2024-03-15 20:07:18 +0100

    new page

--- src/app/sign-in/login-form.tsx
+++ src/app/sign-in/login-form.tsx
@@ -41,7 +41,7 @@ export function LoginForm({ userType }: { userType: string }) {
     onSuccess() {
       const session = JSON.parse(localStorage.getItem('session_key')!)
       if (session.access_token) {
-        router.replace('/dashboard')
+        router.replace('/dc')
       }
     },
   })
